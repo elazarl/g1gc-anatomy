9f92093a9999283dc0b300056fffd18d87c6a3ce
Elazar Leibovich <elazar.leibovich@granulate.io>
2024-07-05 15:04:16 +0300

    jfrust: initial version

---
+++ jfrust/src/jfr.rs
@@ -0,0 +1,222 @@
+use std::time::Duration;
+
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Deserializer};
+
+/*
+{
+ "recording": {
+  "events": []
+ }
+}
+*/
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct JfrMain {
+    pub recording: JfrRecording,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct JfrRecording {
+    pub events: Vec<JfrEvent>,
+}
+/*
+{
+  "type": "jdk.TenuringDistribution",
+  "values": {
+    "startTime": "2024-07-01T09:20:18.758406750+02:00",
+    "gcId": 15,
+    "age": 14,
+    "size": 64
+  }
+}
+*/
+#[derive(Deserialize, Debug, Clone)]
+pub struct TenuringDistribution {
+    #[serde(rename = "startTime", deserialize_with = "deser_ts_ms")]
+    pub start_time: DateTime<Utc>,
+    #[serde(rename = "gcId")]
+    pub gc_id: u64,
+    pub age: u64,
+    pub size: u64,
+}
+#[derive(Deserialize, Debug, Clone)]
+pub enum GCWhen {
+    #[serde(rename = "Before GC")]
+    Before,
+    #[serde(rename = "After GC")]
+    After,
+}
+/*
+"type": "jdk.PromoteObjectOutsidePLAB",
+"values": {
+  "startTime": "2024-07-01T09:20:16.230667208+02:00",
+  "eventThread": {
+    "osName": "Gang worker#6 (Parallel GC Threads)",
+    "osThreadId": 12035,
+    "javaName": null,
+    "javaThreadId": 0,
+    "group": null
+  },
+  "gcId": 0,
+  "objectClass": {
+    "classLoader": {
+      "type": null,
+      "name": "<bootloader>"
+    },
+    "name": "[B",
+    "package": null,
+    "modifiers": 0
+  },
+  "objectSize": 8606,
+  "tenuringAge": 0,
+  "tenured": false
+}
+*/
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct PromoteObjectOutsidePLAB {
+    #[serde(deserialize_with = "deser_ts_ms")]
+    pub start_time: DateTime<Utc>,
+    pub gc_id: u64,
+    pub object_size: u64,
+    pub tenuring_age: u64,
+    pub tenured: bool,
+}
+/*
+"type": "jdk.PromoteObjectInNewPLAB",
+"values": {
+  "startTime": "2024-07-01T09:20:16.230676500+02:00",
+  "eventThread": {
+    "osName": "Gang worker#4 (Parallel GC Threads)",
+    "osThreadId": 11523,
+    "javaName": null,
+    "javaThreadId": 0,
+    "group": null
+  },
+  "gcId": 0,
+  "objectClass": {
+    "classLoader": {
+      "type": null,
+      "name": "<bootloader>"
+    },
+    "name": "java\/lang\/ref\/Reference$ReferenceHandler",
+    "package": {
+      "name": "java\/lang\/ref",
+      "exported": true
+    },
+    "modifiers": 32
+  },
+  "objectSize": 47,
+  "tenuringAge": 0,
+  "tenured": false,
+  "plabSize": 4096
+}
+*/
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct PromoteObjectInNewPLAB {
+    #[serde(deserialize_with = "deser_ts_ms")]
+    pub start_time: DateTime<Utc>,
+    pub gc_id: u64,
+    pub object_size: u64,
+    pub tenuring_age: u64,
+    pub tenured: bool,
+    pub plab_size: u64,
+}
+/*
+"type": "jdk.GCHeapSummary",
+"values": {
+  "startTime": "2024-07-03T13:12:26.119271605+02:00",
+  "gcId": 0,
+  "when": "Before GC",
+  "heapSpace": {
+    "start": 33176944640,
+    "committedEnd": 33285996544,
+    "committedSize": 109051904,
+    "reservedEnd": 33285996544,
+    "reservedSize": 109051904
+  },
+  "heapUsed": 25165824
+}
+*/
+#[derive(Deserialize, Debug, Clone)]
+pub struct GCHeapSummary {
+    #[serde(rename = "startTime", deserialize_with = "deser_ts_ms")]
+    pub start_time: DateTime<Utc>,
+    pub when: GCWhen,
+    #[serde(rename = "gcId")]
+    pub gc_id: u64,
+    #[serde(rename = "heapUsed")]
+    pub heap_used: u64,
+}
+/*
+"startTime": "2024-07-01T09:20:16.230469750+02:00",
+"gcId": 0,
+"when": "Before GC",
+"edenUsedSize": 25165824,
+"edenTotalSize": 25165824,
+"survivorUsedSize": 0,
+"numberOfRegions": 104
+*/
+#[derive(Deserialize, Debug, Clone)]
+pub struct G1HeapSummary {
+    #[serde(rename = "startTime", deserialize_with = "deser_ts_ms")]
+    pub start_time: DateTime<Utc>,
+    pub when: GCWhen,
+    #[serde(rename = "gcId")]
+    pub gc_id: u64,
+    #[serde(rename = "edenUsedSize")]
+    pub eden_used: u64,
+    #[serde(rename = "edenTotalSize")]
+    pub eden_total: u64,
+    #[serde(rename = "survivorUsedSize")]
+    pub survivor_used: u64,
+}
+#[derive(Deserialize, Debug, Clone)]
+#[serde(tag = "type")]
+pub enum JfrEvent {
+    #[serde(rename = "jdk.TenuringDistribution")]
+    TenuringDistribution { values: TenuringDistribution },
+    #[serde(rename = "jdk.GCHeapSummary")]
+    GCHeapSummary { values: GCHeapSummary },
+    #[serde(rename = "jdk.G1HeapSummary")]
+    G1HeapSummary { values: G1HeapSummary },
+    #[serde(rename = "jdk.PromoteObjectOutsidePLAB")]
+    PromoteObjectOutsidePLAB { values: PromoteObjectOutsidePLAB },
+    #[serde(rename = "jdk.PromoteObjectInNewPLAB")]
+    PromoteObjectInNewPLAB { values: PromoteObjectInNewPLAB },
+    #[serde(other)]
+    Unkown,
+}
+
+impl JfrEvent {
+    pub fn gc_id(&self) -> Option<u64> {
+        match &self {
+            JfrEvent::TenuringDistribution { values } => Some(values.gc_id),
+            JfrEvent::GCHeapSummary { values } => Some(values.gc_id),
+            JfrEvent::G1HeapSummary { values } => Some(values.gc_id),
+            JfrEvent::PromoteObjectOutsidePLAB { values } => Some(values.gc_id),
+            JfrEvent::PromoteObjectInNewPLAB { values } => Some(values.gc_id),
+            JfrEvent::Unkown => None,
+        }
+    }
+}
+
+fn deser_ts_ms<'de, D>(deserializer: D) -> Result<DateTime<Utc>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let timestamp = String::deserialize(deserializer)?; // Deserialize as i64
+    DateTime::parse_from_rfc3339(timestamp.as_str())
+        .map(|dt| dt.with_timezone(&Utc))
+        .map_err(|e| serde::de::Error::custom(e.to_string()))
+}
+
+fn deser_dur_ms<'de, D>(deserializer: D) -> Result<Duration, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let ms = u64::deserialize(deserializer)?; // Deserialize as i64
+    Ok(Duration::from_millis(ms))
+}
---
+++ jfrust/src/main.rs
@@ -0,0 +1,193 @@
+pub mod jfr;
+use crate::jfr::{GCHeapSummary, JfrEvent, JfrMain};
+use std::{
+    collections::{BTreeMap, HashMap},
+    io::Write,
+    process::Command,
+    sync::Arc,
+};
+
+use axum::{extract::State, response::Html, routing::get, Json, Router};
+use clap::Parser;
+use itertools::Itertools;
+use plotly::{layout::Legend, plot::Plot, Bar, Candlestick, ImageFormat, Layout, Scatter, Trace};
+use tokio::sync::Mutex;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    jfr_file: String,
+}
+
+struct PlotState {
+    plots: Mutex<Plot>,
+}
+
+type Traces = Vec<Box<dyn Trace + Sync + Send + 'static>>;
+
+#[derive(Default, Clone, Debug)]
+struct Graphs {
+    ages: Vec<Scatter<u64, u64>>,
+    gcs: Vec<Bar<u64, u64>>,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+    let mut cmd = Command::new("jfr");
+    cmd.args(["print", "--json", &args.jfr_file]);
+    let jfr_out = cmd.output().expect("cannnot run jfr");
+    if !jfr_out.status.success() {
+        std::io::stderr().write_all(&jfr_out.stderr).unwrap();
+        return;
+    }
+    let mut graphs: Graphs = Default::default();
+    let jfr_evt: JfrMain = serde_json::from_slice(&jfr_out.stdout).expect("cannot parse jfr JSON");
+    #[derive(Default, Copy, Clone)]
+    struct Candle {
+        gc_id: u64,
+        before_gc: u64,
+        young_before: u64,
+        young_after: u64,
+        survivors_before: u64,
+        survivors_after: u64,
+        after_gc: u64,
+        tenured: u64,
+    }
+    let mut gc_id_to_candle: BTreeMap<u64, Candle> = BTreeMap::new();
+    let var_name = for evt in &jfr_evt.recording.events {
+        let gc_id = evt.gc_id();
+        let candle: &mut Candle = gc_id_to_candle
+            .entry(gc_id.unwrap_or(u64::MAX))
+            .or_insert(Default::default());
+        candle.gc_id = gc_id.unwrap_or(u64::MAX);
+        match &evt {
+            JfrEvent::PromoteObjectOutsidePLAB { values } => {
+                if values.tenured {
+                    candle.tenured += values.object_size;
+                }
+            }
+            JfrEvent::PromoteObjectInNewPLAB { values } => {
+                if values.tenured {
+                    candle.tenured += values.plab_size;
+                }
+            }
+            JfrEvent::G1HeapSummary { values } => match values.when {
+                jfr::GCWhen::Before => {
+                    candle.young_before = values.eden_used;
+                    candle.survivors_before = values.survivor_used;
+                }
+                jfr::GCWhen::After => {
+                    candle.young_after = values.eden_used;
+                    candle.survivors_after = values.survivor_used;
+                }
+            },
+            JfrEvent::GCHeapSummary { values } => match values.when {
+                jfr::GCWhen::Before => candle.before_gc = values.heap_used,
+                jfr::GCWhen::After => {
+                    candle.gc_id = values.gc_id;
+                    candle.after_gc = values.heap_used;
+                }
+            },
+            _ => {}
+        }
+    };
+    let mut gc_ids: Vec<u64> = Vec::new();
+    let mut heap: Vec<u64> = Vec::new();
+    let mut young: Vec<u64> = Vec::new();
+    let mut tenured: Vec<u64> = Vec::new();
+    let mut survivors: Vec<u64> = Vec::new();
+    for (gc_id, candle) in gc_id_to_candle {
+        if gc_id == u64::MAX {
+            continue;
+        }
+        const FACTOR: u64 = 4;
+        let tenured_bytes = candle.tenured;
+        gc_ids.push(candle.gc_id * FACTOR);
+        heap.push(candle.before_gc - candle.young_before);
+        println!(
+            "{} < {} ({} + {})",
+            tenured_bytes,
+            candle.young_before + candle.survivors_before,
+            candle.young_before,
+            candle.survivors_before
+        );
+        assert!(tenured_bytes <= candle.young_before + candle.survivors_before);
+        let young_before = candle.young_before.saturating_sub(tenured_bytes);
+        let survivors_before =
+            candle.survivors_before - tenured_bytes.saturating_sub(candle.young_before);
+
+        young.push(young_before);
+        tenured.push(tenured_bytes);
+        survivors.push(survivors_before);
+
+        gc_ids.push(candle.gc_id * FACTOR + 1);
+        assert!(candle.after_gc >= tenured_bytes);
+        heap.push(candle.after_gc - candle.young_after - tenured_bytes);
+        young.push(candle.young_after);
+        tenured.push(tenured_bytes);
+        survivors.push(candle.survivors_after);
+    }
+    graphs
+        .gcs
+        .push(*Bar::new(gc_ids.clone(), heap).name("heap"));
+    graphs
+        .gcs
+        .push(*Bar::new(gc_ids.clone(), tenured).name("tenured"));
+    graphs
+        .gcs
+        .push(*Bar::new(gc_ids.clone(), young).name("young"));
+    graphs
+        .gcs
+        .push(*Bar::new(gc_ids.clone(), survivors).name("survivors"));
+    let tenure_vec = jfr_evt
+        .recording
+        .events
+        .iter()
+        .filter_map(|e| {
+            if let JfrEvent::TenuringDistribution { values } = e {
+                Some(values)
+            } else {
+                None
+            }
+        })
+        .collect_vec();
+
+    let by_gc_id = tenure_vec.iter().chunk_by(|e| e.gc_id);
+    for (gc_id, tenures) in by_gc_id.into_iter() {
+        let mut ages = Vec::<u64>::new();
+        let mut sizes = Vec::<u64>::new();
+        for tenure in tenures {
+            ages.push(tenure.age);
+            sizes.push(tenure.size);
+        }
+        let trace = Scatter::new(ages, sizes);
+        graphs.ages.push(*trace);
+    }
+    let app = Router::new()
+        .route("/ages", get(ages))
+        .route("/", get(index))
+        .with_state(graphs);
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+    println!("Hello, world!");
+}
+
+async fn ages(State(graphs): State<Graphs>) -> Json<Vec<Plot>> {
+    let mut ages = Plot::new();
+    for trace in graphs.ages {
+        ages.add_trace(Box::new(trace));
+    }
+    let mut gc = Plot::new();
+    for trace in graphs.gcs {
+        gc.add_trace(Box::new(trace));
+    }
+    gc.set_layout(Layout::new().bar_mode(plotly::layout::BarMode::Stack));
+    Json(Vec::from([ages, gc]))
+}
+
+async fn index(State(_state): State<Graphs>) -> Html<&'static str> {
+    Html(include_str!("../assets/index.html"))
+}
